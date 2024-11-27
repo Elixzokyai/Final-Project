52199060771e37eef0b1ee73c5405b304fa68a93
Elixzokyai <elijahmuendo100@gmail.com>
2024-11-27 17:50:56 +0300

    final project

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import BlogUser
+from django import forms
+
+class BlogUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = BlogUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        widgets =  {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter Password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        }
