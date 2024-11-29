3c14cda67ea74d83e7b51c6fe9c57100845584ec
Elixzokyai <elijahmuendo100@gmail.com>
2024-11-30 00:42:31 +0300

    added the admin logs

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import BlogUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(BlogUser)
+class BlogUserAdmin(UserAdmin):
+    model = BlogUser
+    list_display = ('username', 'email', 'date_joined', 'last_login','is_active','is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'email')
+
+    ordering = ('username',)
